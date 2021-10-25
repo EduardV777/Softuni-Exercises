694738dae960125be5873fe785f8e05ce1692b3c
EduardV777 <89710974+EduardV777@users.noreply.github.com>
2021-10-25 16:20:39 +0300

    Add files via upload

---
+++ First Steps in Coding - lab/Projects Creation.py
@@ -0,0 +1,3 @@
+architect_name=input(); nprojects=int(input())
+required_time=3*nprojects
+print(f"The architect {architect_name} will need {required_time} hours to complete {nprojects} project/s.")
