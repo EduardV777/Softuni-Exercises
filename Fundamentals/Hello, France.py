c2336a4dc4390628f419c0ca8ac75d166306a4b5
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2022-01-21 19:22:50 +0200

    Update Hello, France.py

--- Fundamentals/Hello, France.py
+++ Fundamentals/Hello, France.py
@@ -1,55 +1,43 @@
-items=input(); budget=float(input())
-itemsList=[]; profit=0; prices=""; moneySpentOnItems=0
-k=0
-while k<len(items):
-    type=""; price=""
-    if items[k] != "-" and items[k] != ">" and items[k] != "|":
-        if items[k].isalpha()==True:
-            for j in range(k,len(items)):
-                if items[j]!="-" and items[j]!=">" and items[j]!="|":
-                    type+=items[j]
-                    k+=1
-                else:
-                    k+=1
-                    break
-            itemsList.append([type])
-        elif items[k].isdigit()==True or items[k]==".":
-            for j in range(k,len(items)):
-                if items[j]!="-" and items[j]!=">" and items[j]!="|":
-                    price+=items[j]
-                    k+=1
-                else:
-                    k+=1
-                    break
-            itemsList[len(itemsList)-1].append(price)
+collectionOfItems=input(); budget=float(input())
+collectionOfItems=collectionOfItems.split("|"); itemsBought=[]
+profits=0
+for j in range(0,len(collectionOfItems)):
+    collectionOfItems[j]=collectionOfItems[j].split("->")
+    collectionOfItems[j][1]=float(collectionOfItems[j][1])
+
+for k in range(0,len(collectionOfItems)):
+    if collectionOfItems[k][0]=="Clothes":
+        if collectionOfItems[k][1]<=50.00:
+            if budget>=collectionOfItems[k][1]:
+                budget-=collectionOfItems[k][1]
+                itemsBought.append([collectionOfItems[k][0],collectionOfItems[k][1]])
+        else:
+            continue
+    elif collectionOfItems[k][0]=="Shoes":
+        if collectionOfItems[k][1]<=35.00:
+            if budget>=collectionOfItems[k][1]:
+                budget-=collectionOfItems[k][1]
+                itemsBought.append([collectionOfItems[k][0],collectionOfItems[k][1]])
+        else:
+            continue
+    elif collectionOfItems[k][0]=="Accesories":
+        if collectionOfItems[k][1]<=20.50:
+            if budget>=collectionOfItems[k][1]:
+                budget-=collectionOfItems[k][1]
+                itemsBought.append([collectionOfItems[k][0],collectionOfItems[k][1]])
+        else:
+            continue
+output=""
+for j in range(0,len(itemsBought)):
+    profits += itemsBought[j][1] * 0.40
+    itemsBought[j][1]+=(itemsBought[j][1])*0.40
+    budget+=itemsBought[j][1]
+    if j!=len(itemsBought)-1:
+        output+=f"{round(itemsBought[j][1],2)} "
     else:
-        k+=1
-for k in range(0,len(itemsList)):
-    for j in range(0,1):
-        if itemsList[k][j]=="Clothes":
-            if float(itemsList[k][j+1])<=50.00:
-                if budget>=float(itemsList[k][j+1]):
-                    budget-=float(itemsList[k][j+1])
-                    moneySpentOnItems += float(itemsList[k][j + 1])
-                    prices += str(round(float(itemsList[k][j + 1]) + (float(itemsList[k][j + 1]) * 0.4),2))+" "
-                    profit+=float(itemsList[k][j+1])+(float(itemsList[k][j+1])*0.4)
-        elif itemsList[k][j]=="Shoes":
-            if float(itemsList[k][j + 1]) <= 35.00:
-                if budget>=float(itemsList[k][j+1]):
-                    budget-=float(itemsList[k][j+1])
-                    moneySpentOnItems += float(itemsList[k][j + 1])
-                    prices += str(round(float(itemsList[k][j + 1]) + (float(itemsList[k][j + 1]) * 0.4),2))+" "
-                    profit += float(itemsList[k][j + 1]) + (float(itemsList[k][j + 1]) * 0.4)
-        elif itemsList[k][j]=="Accessories":
-            if float(itemsList[k][j + 1]) <= 20.50:
-                if budget>=float(itemsList[k][j+1]):
-                    budget -= float(itemsList[k][j + 1])
-                    moneySpentOnItems+=float(itemsList[k][j + 1])
-                    prices += str(round(float(itemsList[k][j + 1]) + (float(itemsList[k][j + 1]) * 0.4),2))+" "
-                    profit+=round(float(itemsList[k][j + 1]) + (float(itemsList[k][j + 1]) * 0.4),2)
-budget+=profit
-profit-=moneySpentOnItems
-print(f"{prices}\nProfit: {profit:.2f}")
+        output += f"{round(itemsBought[j][1], 2)}"
+print(output)
+print(f"Profit: {profits:.2f}")
 if budget>=150:
     print("Hello, France!")
 else:
