1ea43bb43483ca6c80be327a48ae0c7a882f1dd9
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2022-01-24 13:38:43 +0200

    Update Groups of 10s.py

--- Fundamentals/Lists lab-exercise(Advanced)/Lists Exercises/Groups of 10s.py
+++ Fundamentals/Lists lab-exercise(Advanced)/Lists Exercises/Groups of 10s.py
@@ -1,33 +1,17 @@
+from math import ceil
 numbers=input()
-numbersL=[]
-k=0
-while k<len(numbers):
-    num=""
-    if numbers[k]!=" ":
-        for j in range(k,len(numbers)):
-            if numbers[j]!=",":
-                num+=numbers[j]
-                k+=1
-            else:
-                k+=1
-                break
-        num=int(num)
-        numbersL.append(num)
-    else:
-        k+=1
-maxVal=max(numbersL); output=""
-groups10s=list(filter(lambda x: 1<=x<=10,numbersL))
-output+=f"Group of 10's: {groups10s}\n"
-if maxVal>10:
-    groups20s=list(filter(lambda x: 11<=x<=20,numbersL))
-    output+=f"Group of 20's: {groups20s}\n"
-if maxVal>20:
-    groups30s=list(filter(lambda x: 21<=x<=30,numbersL))
-    output+=f"Group of 30's: {groups30s}\n"
-if maxVal>30:
-    groups40s=list(filter(lambda x: 31<=x<=40,numbersL))
-    output+=f"Group of 40's: {groups40s}\n"
-if maxVal>40:
-    groups50s=list(filter(lambda x: 41<=x<=50,numbersL))
-    output+=f"Group of 50's: {groups50s}"
-print(output)
+numbers=numbers.split(", ")
+numbers=[int(e) for e in numbers]
+groupVal=10
+boundary=ceil(max(numbers)/10)
+groups=[]
+while len(numbers)!=0:
+    groups.append(list(filter(lambda x: groupVal-10<x<=groupVal,numbers)))
+    j=0
+    while j<len(numbers):
+        if groupVal-10<numbers[j]<=groupVal:
+            del numbers[j]
+        else:
+            j+=1
+    print(f"Group of {groupVal}'s: {groups[len(groups)-1]}")
+    groupVal+=10
