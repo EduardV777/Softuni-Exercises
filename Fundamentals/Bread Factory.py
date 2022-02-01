3b8c9f2579b444a33748e0ee98c810cbe55b1e9b
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2022-02-01 02:54:49 +0200

    Update Bread Factory.py

--- Fundamentals/Bread Factory.py
+++ Fundamentals/Bread Factory.py
@@ -1,34 +1,28 @@
-workEvents=input()
-workEvents=workEvents.split("|")
-energy=100; coins=100; closedBakery=False
-for k in range(0,len(workEvents)):
-    workEvents[k]=workEvents[k].split("-")
-    workEvents[k][1]=int(workEvents[k][1])
-
-for j in range(0,len(workEvents)):
-    if workEvents[j][0]=="rest":
-        if energy+workEvents[j][1]>100:
-            energy=100
-            print(f"You gained 0 energy.")
-        else:
-            print(f"You gained {workEvents[j][1]} energy.")
-            energy+=workEvents[j][1]
+events=input()
+events=events.split("|")
+energy=100; coins=100; handledEverything=True
+for k in range(0,len(events)):
+    currEvent=events[k].split("-")
+    if currEvent[0]=="rest":
+        currEnergy=energy
+        if energy+int(currEvent[1])<=100:
+            energy+=int(currEvent[1])
+        print(f"You gained {energy-currEnergy} energy.")
         print(f"Current energy: {energy}.")
-    elif workEvents[j][0]=="order":
+    elif currEvent[0]=="order":
         if energy>=30:
-            print(f"You earned {workEvents[j][1]} coins.")
-            coins += workEvents[j][1]
+            coins += int(currEvent[1])
             energy-=30
+            print(f"You earned {int(currEvent[1])} coins.")
         else:
-            print("You had to rest!")
             energy+=50
+            print("You had to rest!")
     else:
-        if coins>=workEvents[j][1]:
-            print(f"You bought {workEvents[j][0]}.")
-            coins-=workEvents[j][1]
+        if coins>=int(currEvent[1]):
+            print(f"You bought {currEvent[0]}.")
+            coins-=int(currEvent[1])
         else:
-            print(f"Closed! Cannot afford {workEvents[j][0]}.")
-            closedBakery=True
-            break
-if closedBakery==False:
+            print(f"Closed! Cannot afford {currEvent[0]}.")
+            handledEverything=False; break
+if handledEverything==True:
     print(f"Day completed!\nCoins: {coins}\nEnergy: {energy}")
