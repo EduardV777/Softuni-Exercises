f0239d72c5b6018312bddfd2897f13e7a805b736
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2022-02-01 02:38:48 +0200

    Update Hello, France.py

--- Fundamentals/Hello, France.py
+++ Fundamentals/Hello, France.py
@@ -1,43 +1,28 @@
-collectionOfItems=input(); budget=float(input())
-collectionOfItems=collectionOfItems.split("|"); itemsBought=[]
-profits=0
-for j in range(0,len(collectionOfItems)):
-    collectionOfItems[j]=collectionOfItems[j].split("->")
-    collectionOfItems[j][1]=float(collectionOfItems[j][1])
-
-for k in range(0,len(collectionOfItems)):
-    if collectionOfItems[k][0]=="Clothes":
-        if collectionOfItems[k][1]<=50.00:
-            if budget>=collectionOfItems[k][1]:
-                budget-=collectionOfItems[k][1]
-                itemsBought.append([collectionOfItems[k][0],collectionOfItems[k][1]])
-        else:
-            continue
-    elif collectionOfItems[k][0]=="Shoes":
-        if collectionOfItems[k][1]<=35.00:
-            if budget>=collectionOfItems[k][1]:
-                budget-=collectionOfItems[k][1]
-                itemsBought.append([collectionOfItems[k][0],collectionOfItems[k][1]])
-        else:
-            continue
-    elif collectionOfItems[k][0]=="Accesories":
-        if collectionOfItems[k][1]<=20.50:
-            if budget>=collectionOfItems[k][1]:
-                budget-=collectionOfItems[k][1]
-                itemsBought.append([collectionOfItems[k][0],collectionOfItems[k][1]])
-        else:
-            continue
-output=""
-for j in range(0,len(itemsBought)):
-    profits += itemsBought[j][1] * 0.40
-    itemsBought[j][1]+=(itemsBought[j][1])*0.40
-    budget+=itemsBought[j][1]
-    if j!=len(itemsBought)-1:
-        output+=f"{round(itemsBought[j][1],2)} "
-    else:
-        output += f"{round(itemsBought[j][1], 2)}"
-print(output)
-print(f"Profit: {profits:.2f}")
+collection=input(); budget=int(input())
+collection=collection.split("|")
+boughtItems=[]; initialBudget=budget
+for k in range(0,len(collection)):
+    currItem=collection[k].split("->")
+    boughtSomething = False
+    if currItem[0]=="Clothes":
+        if float(currItem[1])<=50.00:
+            boughtSomething = True
+    elif currItem[0]=="Shoes":
+        if float(currItem[1])<=35.00:
+            boughtSomething = True
+    elif currItem[0]=="Accessories":
+        if float(currItem[1])<=20.50:
+            boughtSomething = True
+    if boughtSomething==True and budget>=float(currItem[1]):
+        boughtItems.append(float(currItem[1]) + (float(currItem[1]) * 0.4))
+        budget-=float(currItem[1])
+printNewPrices=""
+for k in range(0,len(boughtItems)):
+    printNewPrices+=f"{boughtItems[k]:.2f}"
+    if k!=len(boughtItems)-1:
+        printNewPrices+=" "
+    budget+=boughtItems[k]
+print(f"{printNewPrices}\nProfit: {budget-initialBudget:.2f}")
 if budget>=150:
     print("Hello, France!")
 else:
