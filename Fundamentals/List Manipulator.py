368e0502b828fc2b04869fc6d18189a90b8881e7
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2022-02-02 16:33:07 +0200

    Update List Manipulator.py

--- Fundamentals/List Manipulator.py
+++ Fundamentals/List Manipulator.py
@@ -1,131 +1,82 @@
-list1=input()
-list1=list1.split(" ")
-for k in range(0,len(list1)):
-    list1[k]=int(list1[k])
+data=input()
+data=data.split(" "); data=[int(e) for e in data]
+
 while True:
     command=input()
     if command!="end":
         if "exchange" in command:
             command=command.split(" ")
-            if -1<int(command[1])<len(list1):
-                subList=[]; k=int(command[1])+1
-                while k<len(list1):
-                    subList.append(list1[k])
-                    del list1[k]
-                subList.reverse()
-                for k in range(0,len(subList)):
-                    list1.insert(0,subList[k])
-            else:
+            if int(command[1])<0 or int(command[1])>len(data):
                 print("Invalid index")
-        elif "max" in command:
-            if "odd" in command:
-                oddElements=[]
-                for k in range(0,len(list1)):
-                    if list1[k]%2!=0:
-                        oddElements.append(list1[k])
-                if len(oddElements)>0:
-                    maxNum=max(oddElements)
-                    ind=list1.index(maxNum)
-                    print(ind)
+                continue
+            subList1=data[:int(command[1])+1]
+            subList2=data[int(command[1])+1:]
+            data=list(subList2+subList1)
+        elif "max" in command or "min" in command:
+            command=command.split(" ")
+            lookForMin=False; noMatches=False
+            if command[0]=="min":
+                lookForMin=True
+            if command[1]=="even":
+                evenNumbers = [e for e in data if e % 2 == 0]
+                if lookForMin==False:
+                    if len(evenNumbers)==0:
+                        noMatches=True
+                    else:
+                        ind = data.index(max(evenNumbers))
                 else:
-                    print("No matches")
-            elif "even" in command:
-                evenElements=[]
-                for k in range(0,len(list1)):
-                    if list1[k]%2==0:
-                        evenElements.append(list1[k])
-                if len(evenElements)>0:
-                    maxNum=max(evenElements)
-                    ind=list1.index(maxNum)
-                    print(ind)
+                    if len(evenNumbers)==0:
+                        noMatches=True
+                    else:
+                        ind = data.index(min(evenNumbers))
+            elif command[1]=="odd":
+                oddNumbers = [e for e in data if e % 2 != 0]
+                if lookForMin==False:
+                    if len(oddNumbers)==0:
+                        noMatches=True
+                    else:
+                        ind = data.index(max(oddNumbers))
                 else:
-                    print("No matches")
-        elif "min" in command:
-            if "odd" in command:
-                oddElements = []
-                for k in range(0, len(list1)):
-                    if list1[k]%2!=0:
-                        oddElements.append(list1[k])
-                if len(oddElements)>0:
-                    minNum = min(oddElements)
-                    ind = list1.index(minNum)
-                    print(ind)
-                else:
-                    print("No matches")
-            elif "even" in command:
-                evenElements = []
-                for k in range(0, len(list1)):
-                    if list1[k]%2==0:
-                        evenElements.append(list1[k])
-                if len(evenElements)>0:
-                    minNum = min(evenElements)
-                    ind = list1.index(minNum)
-                    print(ind)
-                else:
-                    print("No matches")
-        elif "first" in command:
-            if "odd" in command:
-                command=command.split(" "); oddElements=[]
-                count=int(command[1])
-                if count>=len(list1):
+                    if len(oddNumbers)==0:
+                        noMatches=True
+                    else:
+                        ind = data.index(min(oddNumbers))
+            if noMatches==True:
+                print("No matches")
+            else:
+                print(ind)
+        elif "first" in command or "last" in command:
+            command=command.split(" ")
+            lookForLast=False; lookForEven=False
+            elements=[]
+            if command[0]=="last":
+                lookForLast=True
+            if command[0]=="first" or command[0]=="last":
+                if int(command[1])>len(data) or int(command[1])<0:
                     print("Invalid count")
+                    continue
                 else:
-                    k=0
-                    while k<len(list1):
-                        if count==0:
-                            break
-                        if list1[k]%2!=0:
-                            oddElements.append(list1[k])
-                            count-=1
-                        k+=1
-                    print(oddElements)
-            elif "even" in command:
-                command = command.split(" "); evenElements = []
-                count = int(command[1])
-                if count>=len(list1):
-                    print("Invalid count")
-                else:
-                    k = 0
-                    while k < len(list1):
-                        if count==0:
-                            break
-                        if list1[k] % 2 == 0:
-                            evenElements.append(list1[k])
-                            count -= 1
-                        k+=1
-                    print(evenElements)
-
-        elif "last" in command:
-            if "odd" in command:
-                command=command.split(" "); oddElements=[]
-                count=int(command[1])
-                if len(list1)-count<0:
-                    print("Invalid count")
-                else:
-                    k=len(list1)-1
-                    while k>=0:
-                        if count==0:
-                            break
-                        if list1[k]%2!=0:
-                            oddElements.append(list1[k])
-                            count-=1
-                        k-=1
-                    print(oddElements)
-            elif "even" in command:
-                command = command.split(" "); evenElements = []
-                count = int(command[1])
-                if len(list1)-count<0:
-                    print("Invalid count")
-                else:
-                    k = len(list1)-1
-                    while k >= 0:
-                        if count==0:
-                            break
-                        if list1[k] % 2 == 0:
-                            evenElements.append(list1[k])
-                            count -= 1
-                        k-=1
-                    print(evenElements)
+                    if command[2]=="even":
+                        lookForEven=True
+                    if lookForEven==True:
+                        if lookForLast==True:
+                            for k in range(len(data)-1, len(data)-int(command[1])-2,-1):
+                                if data[k] % 2 == 0:
+                                    elements.append(data[k])
+                        else:
+                            for k in range(0, 0+int(command[1])):
+                                if data[k] % 2 == 0:
+                                    elements.append(data[k])
+                    else:
+                        if lookForLast==True:
+                            for k in range(len(data)-1, len(data)-int(command[1])-2,-1):
+                                if data[k] % 2 != 0:
+                                    elements.append(data[k])
+                        else:
+                            for k in range(0, 0+int(command[1])):
+                                if data[k] % 2 != 0:
+                                    elements.append(data[k])
+                print(elements)
     else:
-        print(list1)
+        print(data)
         break
