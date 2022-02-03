b316148d2fabb16b5f2d55aec039267fbb470ce0
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2022-02-03 14:11:13 +0200

    Update List Manipulator.py

--- Fundamentals/List Manipulator.py
+++ Fundamentals/List Manipulator.py
@@ -23,24 +23,28 @@ while True:
                     if len(evenNumbers)==0:
                         noMatches=True
                     else:
-                        ind = data.index(max(evenNumbers))
+                        maxNum=max(evenNumbers)
+                        ind = data.index(maxNum)
                 else:
                     if len(evenNumbers)==0:
                         noMatches=True
                     else:
-                        ind = data.index(min(evenNumbers))
+                        minNum=min(evenNumbers)
+                        ind = data.index(minNum)
             elif command[1]=="odd":
                 oddNumbers = [e for e in data if e % 2 != 0]
                 if lookForMin==False:
                     if len(oddNumbers)==0:
                         noMatches=True
                     else:
-                        ind = data.index(max(oddNumbers))
+                        maxNum=max(oddNumbers)
+                        ind = data.index(maxNum)
                 else:
                     if len(oddNumbers)==0:
                         noMatches=True
                     else:
-                        ind = data.index(min(oddNumbers))
+                        minNum=min(oddNumbers)
+                        ind = data.index(minNum)
             if noMatches==True:
                 print("No matches")
             else:
@@ -51,42 +55,50 @@ while True:
             elements=[]; k=0
             if command[0]=="last":
                 lookForLast=True
-            if int(command[1])>len(data) or int(command[1])<0:
-                print("Invalid count")
-                continue
-            else:
-                if command[2]=="even":
-                    lookForEven=True
-                if lookForEven==True:
-                    if lookForLast==True:
-                        for j in range(len(data)-1, -1,-1):
-                            if k==int(command[1]):
-                                break
-                            if data[j] % 2 == 0:
-                                elements.append(data[j])
-                                k+=1
-                    else:
-                        for j in range(0, len(data)):
-                            if k==int(command[1]):
-                                break
-                            if data[j] % 2 == 0:
-                                elements.append(data[j])
-                                k+=1
+            if command[2]=="even":
+                lookForEven=True
+            if lookForEven==True:
+                if lookForLast==True:
+                    if (len(data)-1)-int(command[1])<0:
+                        print("Invalid count")
+                        continue
+                    for j in range(len(data)-1, -1,-1):
+                        if k==int(command[1]):
+                            break
+                        if data[j] % 2 == 0:
+                            elements.append(data[j])
+                            k+=1
                 else:
-                    if lookForLast==True:
-                        for j in range(len(data)-1, -1,-1):
-                            if k==int(command[1]):
-                                break
-                            if data[j] % 2 != 0:
-                                elements.append(data[j])
-                                k+=1
-                    else:
-                        for j in range(0, len(data)):
-                            if k==int(command[1]):
-                                break
-                            if data[j] % 2 != 0:
-                                elements.append(data[j])
-                                k+=1
+                    if int(command[1])>len(data)-1:
+                        print("Invalid count")
+                        continue
+                    for j in range(0, len(data)):
+                        if k==int(command[1]):
+                            break
+                        if data[j] % 2 == 0:
+                            elements.append(data[j])
+                            k+=1
+            else:
+                if lookForLast==True:
+                    if (len(data)-1)-int(command[1])<0:
+                        print("Invalid count")
+                        continue
+                    for j in range(len(data)-1, -1,-1):
+                        if k==int(command[1]):
+                            break
+                        if data[j] % 2 != 0:
+                            elements.append(data[j])
+                            k+=1
+                else:
+                    if int(command[1])>len(data)-1:
+                        print("Invalid count")
+                        continue
+                    for j in range(0, len(data)):
+                        if k==int(command[1]):
+                            break
+                        if data[j] % 2 != 0:
+                            elements.append(data[j])
+                            k+=1
             print(elements)
     else:
         print(data)
