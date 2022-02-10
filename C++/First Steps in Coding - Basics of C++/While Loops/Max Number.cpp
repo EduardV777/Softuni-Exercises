ee815a37b4cfcb0a96655063a71c985513bdfe08
Eduard Velkov <edwardthedriver@outlook.com>
2022-02-10 17:42:22 +0200

    Create Max Number.cpp

---
+++ C++/First Steps in Coding - Basics of C++/While Loops/Max Number.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int maxNum, k=0;
+	while (true) {
+		string currNum;
+		cin >> currNum;
+		if (currNum=="Stop"){
+			cout << maxNum;
+			break;
+		}
+		else {
+			int number = stod(currNum);
+			if (k == 0) {
+				maxNum = number;
+				k++;
+			}
+			if (maxNum < number) {
+				maxNum = number;
+			}
+		}
+	}
+}
