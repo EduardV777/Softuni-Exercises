87e09ed8a97999a6721cd3b2c597206859d2badf
Eduard Velkov <edwardthedriver@outlook.com>
2022-02-12 00:32:07 +0200

    Update Town.py

--- Python/Fundamentals/Objects And Classes(lab-exercise)/Exercises/Town.py
+++ Python/Fundamentals/Objects And Classes(lab-exercise)/Exercises/Town.py
@@ -1,15 +1,15 @@
 class Town:
-    longi=0; lati=0
     def __init__(self,name):
-        self.townName=name
+        self.name=name
+        self.latitude="0°N"
+        self.longitude="0°E"
     def set_latitude(self,latitude):
-        self.lati = latitude
+        self.latitude=latitude
     def set_longitude(self,longitude):
-        self.longi = longitude
+        self.longitude=longitude
     def __repr__(self):
-        return f"Town: {self.townName} | Latitude: {self.lati} | Longitude: {self.longi}"
-
-town = Town("Sofia")
-town.set_latitude("42° 41\' 51.04\" N")
-town.set_longitude("23° 19\' 26.94\" E")
-print(town)
+        return f"Town: {self.name} | Latitude: {self.latitude} | Longitude: {self.longitude}"
+# town = Town("Sofia")
+# town.set_latitude("42° 41\' 51.04\" N")
+# town.set_longitude("23° 19\' 26.94\" E")
+# print(town)
