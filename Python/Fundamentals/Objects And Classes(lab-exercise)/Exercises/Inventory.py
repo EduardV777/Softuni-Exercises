eb1fb8a666b9f285c87dbe612fee7a44dedaa79d
Eduard Velkov <edwardthedriver@outlook.com>
2022-02-12 00:56:02 +0200

    Update Inventory.py

--- Python/Fundamentals/Objects And Classes(lab-exercise)/Exercises/Inventory.py
+++ Python/Fundamentals/Objects And Classes(lab-exercise)/Exercises/Inventory.py
@@ -1,22 +1,21 @@
 class Inventory:
     items=[]
-    def __init__(self, __capacity: int):
-        self.__cap=__capacity
-        self.capCount=self.__cap
-    def add_item(self,item: str):
-        if self.capCount>0:
+    def __init__(self, capacity: int):
+        self.__capacity=capacity
+    def add_item(self, item: str):
+        if len(self.items)<self.__capacity:
             self.items.append(item)
-            self.capCount-=1
         else:
             return "not enough room in the inventory"
     def get_capacity(self):
-        return self.__cap
+        return self.__capacity
     def __repr__(self):
-        return f"Items: {', '.join(self.items)}\nCapacity left: {self.capCount}"
+        items=', '.join(self.items)
+        return f"Items: {items}.\nCapacity left: {self.__capacity-len(self.items)}"
 
-inventory = Inventory(2)
-inventory.add_item("potion")
-inventory.add_item("sword")
-print(inventory.add_item("bottle"))
-print(inventory.get_capacity())
-print(inventory)
+# inventory = Inventory(2)
+# inventory.add_item("potion")
+# inventory.add_item("sword")
+# print(inventory.add_item("bottle"))
+# print(inventory.get_capacity())
+# print(inventory)
