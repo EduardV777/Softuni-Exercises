27b52284a4c3735fc4061796eb168080713fe8e0
Eduard Velkov <edwardthedriver@outlook.com>
2022-02-12 00:49:10 +0200

    Update Class.py

--- Python/Fundamentals/Objects And Classes(lab-exercise)/Exercises/Class.py
+++ Python/Fundamentals/Objects And Classes(lab-exercise)/Exercises/Class.py
@@ -1,24 +1,24 @@
 class Class:
-    students = []
-    grades=[]
     __students_count=22
-    def __init__(self, name):
-        self.className=name
-    def add_student(self,nameStr: str,grade: float):
-        if self.__students_count>0:
-            self.students.append(nameStr)
-            self.grades.append(grade)
+    def __init__(self,name):
+        self.name=name
+        self.students=[]
+        self.grades=[]
+    def add_student(self,name: str, grade: float):
+        if len(self.students)<self.__students_count:
+            self.students.append(name); self.grades.append(grade)
     def get_average_grade(self):
-        gradeCount=len(self.grades); gradeSum=0
+        avgGrades=0
         for k in range(0,len(self.grades)):
-            gradeSum+=self.grades[k]
-        avg=gradeSum/gradeCount
-        return f"{avg:.2f}"
+            avgGrades+=self.grades[k]
+        avgGrades/=len(self.grades)
+        return f"{avgGrades:.2f}"
     def __repr__(self):
-        return f"The students in {self.className}: {', '.join(self.students)}.\nAverage grade: {self.get_average_grade()}"
+        avgGrade=self.get_average_grade()
+        return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {avgGrade}"
 
-a_class = Class("11B")
-a_class.add_student("Peter", 4.80)
-a_class.add_student("George", 6.00)
-a_class.add_student("Amy", 3.50)
-print(a_class)
+# a_class = Class("11B")
+# a_class.add_student("Peter", 4.80)
+# a_class.add_student("George", 6.00)
+# a_class.add_student("Amy", 3.50)
+# print(a_class)
