aad4a8f493f73643b6ef24a7bd0fba23fc33b45b
Eduard Velkov <edwardthedriver@outlook.com>
2022-02-16 14:15:47 +0200

    Create Clock.cpp

---
+++ C++/First Steps in Coding - Basics of C++/Nested Loops/Clock.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (int k = 0; k <= 23; k++) {
+		for (int j = 0; j <= 59; j++) {
+			cout << k << ":" << j << endl;
+		}
+	}
+	return 0;
+}
