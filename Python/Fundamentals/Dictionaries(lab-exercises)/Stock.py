ac2eca4bbd9c40b73665f17200be8a446f90ca23
Eduard Velkov <velkovmail@yandex.ru>
2022-03-04 21:35:14 +0200

    Update Stock.py

--- Python/Fundamentals/Dictionaries(lab-exercises)/Stock.py
+++ Python/Fundamentals/Dictionaries(lab-exercises)/Stock.py
@@ -1,45 +1,12 @@
-string=input()
-k=0; i=0
-keys=[]; values=[]; productsToLookFor=[]
+productsAndQty=input(); productsToSearch=input()
+productsAndQty=productsAndQty.split(" "); productsToSearch=productsToSearch.split(" ")
+products=[e for e in productsAndQty if e.isdigit()==False]
+quantities=[int(e) for e in productsAndQty if e.isdigit()==True]
+storage=dict(zip(products,quantities))
 
-while k<len(string):
-    if string[k]!=" ":
-        val=""
-        for j in range(k,len(string)):
-            if string[j]!=" ":
-                val+=string[j]
-                k+=1
-            else:
-                break
-        if i==0:
-            keys.append(val)
-            i+=1
-        else:
-            values.append(val)
-            i-=1
+for k in range(0,len(productsToSearch)):
+    available=storage.get(productsToSearch[k], "no")
+    if available=="no":
+        print(f"Sorry, we don't have {productsToSearch[k]}")
     else:
-        k+=1
-
-stocks=dict(zip(keys,values))
-searchingFor=input()
-k=0
-
-while k<len(searchingFor):
-    if searchingFor[k]!=" ":
-        val=""
-        for j in range(k,len(searchingFor)):
-            if searchingFor[j]!=" ":
-                val+=searchingFor[j]
-                k+=1
-            else:
-                break
-        productsToLookFor.append(val)
-    else:
-        k+=1
-
-for k in range(0,len(productsToLookFor)):
-    if stocks.get(productsToLookFor[k])!=None:
-        msg=stocks.get(productsToLookFor[k])
-        print(f"We have {msg} of {productsToLookFor[k]} left")
-    else:
-        print(f"Sorry, we don't have {productsToLookFor[k]}")
+        print(f"We have {available} of {productsToSearch[k]} left")
