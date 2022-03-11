2a3f74b4649d746685e5883ff61aac04abbe6800
Eduard Velkov <velkovmail@yandex.ru>
2022-03-11 14:01:08 +0300

    Update Student Academy.py

--- Python/Fundamentals/Dictionaries(lab-exercises)/Exercises/Student Academy.py
+++ Python/Fundamentals/Dictionaries(lab-exercises)/Exercises/Student Academy.py
@@ -1,21 +1,23 @@
-studentsData={}
 n=int(input())
+students={}
 for k in range(0,n):
-    studentName=input(); studentGrade=float(input())
-    studentFound=False
-    for j in studentsData:
-        if studentName==j:
-            studentFound=True
-            studentsData[j][0]+=studentGrade; studentsData[j][1]+=1
-            break
-    if studentFound==False:
-        studentsData[studentName]=[studentGrade, 1]
+    name = input()
+    grade=float(input())
+    if not name in students:
+        students[name]=[grade]
+    else:
+        students[name].append(grade)
 
-for j in studentsData:
-    studentsData[j]=studentsData[j][0]/studentsData[j][1]
-    
-orderByAvgGrade=sorted(studentsData.items(),key=lambda x: x[1], reverse=True)
-orderedStudentsData=dict(orderByAvgGrade)
-for j in orderedStudentsData:
-    if orderedStudentsData[j]>=4.5:
-        print(f"{j} -> {orderedStudentsData[j]:.2f}")
+for j in students:
+    gradesCount=len(students[j]); gradesSum=0
+    for k in range(0,len(students[j])):
+        gradesSum+=students[j][k]
+    students[j]=gradesSum/gradesCount
+    if students[j]<4.50:
+        students[j]="Doesn't pass"
+
+for j in students:
+    if students[j]=="Doesn't pass":
+        continue
+    else:
+        print(f"{j} -> {students[j]:.2f}")
