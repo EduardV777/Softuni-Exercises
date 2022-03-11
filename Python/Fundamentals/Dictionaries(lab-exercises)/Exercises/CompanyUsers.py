5f5dd895e62a9e6bdad33bcde0bdff8f32dc4097
Eduard Velkov <velkovmail@yandex.ru>
2022-03-11 14:19:48 +0300

    Update CompanyUsers.py

--- Python/Fundamentals/Dictionaries(lab-exercises)/Exercises/CompanyUsers.py
+++ Python/Fundamentals/Dictionaries(lab-exercises)/Exercises/CompanyUsers.py
@@ -1,27 +1,16 @@
-companies_employees={}
-
+companies={}
 while True:
-    command=input()
-    if command!="End":
-        companyFound=False
-        employeeData=command.split(" -> ")
-        companyName=employeeData[0]; employeeId=employeeData[1]
-        for j in companies_employees:
-            if j==companyName:
-                companyFound=True
-                try:
-                    if companies_employees[j].index(employeeId)>-1:
-                        break
-                except:
-                    companies_employees[j].append(employeeId)
-                break
-        if companyFound==False:
-            companies_employees[companyName]=[employeeId]
+    data=input()
+    if data!="End":
+        data=data.split(" -> ")
+        if data[0] in companies:
+            if not data[1] in companies[data[0]]:
+                companies[data[0]].append(data[1])
+        else:
+            companies[data[0]]=[data[1]]
     else:
-        orderByCompany = sorted(companies_employees.items())
-        orderByCompany = dict(orderByCompany)
-        for j in orderByCompany:
+        for j in companies:
             print(f"{j}")
-            for k in range(0,len(orderByCompany[j])):
-                print(f"-- {orderByCompany[j][k]}")
+            for k in range(0,len(companies[j])):
+                print(f"-- {companies[j][k]}")
         break
