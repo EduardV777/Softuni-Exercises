6fd591951a729c7ac793b3ec2050154b22bcab11
Eduard Velkov <velkovmail@yandex.ru>
2022-03-11 23:13:43 +0300

    Update Winning Tickets.py

--- Python/Fundamentals/Text Processing(lab-exercises)/Exercises/Winning Tickets.py
+++ Python/Fundamentals/Text Processing(lab-exercises)/Exercises/Winning Tickets.py
@@ -9,15 +9,17 @@ for k in range(0,len(collectionOfTickets)):
         print("invalid ticket")
         continue
     else:
-        jackpotCheck=["",0,0]
-        firstHalf=0; secondHalf=0; winningSymbolsDetected=False
+        jackpotCheck=["@",0,0]
+        firstHalf=0; secondHalf=0; newSymbolFound=False
         symbs=0; half="first"
         for j in range(0,len(collectionOfTickets[k])):
-            if j>=len(collectionOfTickets[k])//2 and half=="first":
+            if j>=len(collectionOfTickets[k])/2 and half=="first":
                 half="second"; symbs=0
             if len(collectionOfTickets[k])==20:
                 if collectionOfTickets[k][j]=="@" or collectionOfTickets[k][j]=="#" or collectionOfTickets[k][j]=="$" or collectionOfTickets[k][j]=="^":
-                    jackpotCheck[0]=collectionOfTickets[k][j]
+                    if jackpotCheck[0]!=collectionOfTickets[k][j]:
+                        symbs=0
+                        jackpotCheck[0]=collectionOfTickets[k][j]
                     symbs+=1
                     if half=="first":
                         if symbs>firstHalf:
@@ -33,6 +35,11 @@ for k in range(0,len(collectionOfTickets)):
             print(f"ticket \"{collectionOfTickets[k]}\" - {jackpotCheck[1]}{jackpotCheck[0]} Jackpot!")
         else:
             if firstHalf>=6 and secondHalf>=6:
-                print(f"ticket \"{collectionOfTickets[k]}\" - {jackpotCheck[1]}{jackpotCheck[0]}")
+                #If there are more on any one side, we adjust the value of second half to display the correct amount of matching symbols on both sides
+                if secondHalf>firstHalf:
+                    secondHalf=firstHalf
+                if firstHalf>secondHalf:
+                    firstHalf=secondHalf
+                print(f"ticket \"{collectionOfTickets[k]}\" - {secondHalf}{jackpotCheck[0]}")
             else:
                 print(f"ticket \"{collectionOfTickets[k]}\" - no match")
