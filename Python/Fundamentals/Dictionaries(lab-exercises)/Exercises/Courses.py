7af5ee693751c91b0a2df0dc115ab152f30388b9
Eduard Velkov <velkovmail@yandex.ru>
2022-03-11 13:31:34 +0300

    Update Courses.py

--- Python/Fundamentals/Dictionaries(lab-exercises)/Exercises/Courses.py
+++ Python/Fundamentals/Dictionaries(lab-exercises)/Exercises/Courses.py
@@ -1,24 +1,21 @@
 courses={}
+
 while True:
     command=input()
     if command!="end":
-        studentData=command.split(" : ")
-        courseFound=False
+        command=command.split(" : ")
+        doesCourseExist=False
         for j in courses:
-            if j==studentData[0]:
-                courseFound=True
-                courses[j][0]+=1; courses[j][1].append(studentData[1])
+            if j==command[0]:
+                doesCourseExist=True
                 break
-        if courseFound==False:
-            courses[studentData[0]]=[1,[studentData[1]]]
+        if doesCourseExist==False:
+            courses[command[0]]=[command[1]]
+        else:
+            courses[command[0]].append(command[1])
     else:
-        sortByRegStudents=sorted(courses.items(),key=lambda x: x[1], reverse=True)
-        sortedByRegStudents=dict(sortByRegStudents)
-        output=""
-        for j in sortedByRegStudents:
-            output+=f"{j}: {courses[j][0]}\n"
-            courses[j][1].sort()
-            for k in range(0,len(courses[j][1])):
-                output+=f"-- {courses[j][1][k]}\n"
-        print(output)
+        for j in courses:
+            print(f"{j}: {len(courses[j])}")
+            for k in range(0,len(courses[j])):
+                print(f"-- {courses[j][k]}")
         break
