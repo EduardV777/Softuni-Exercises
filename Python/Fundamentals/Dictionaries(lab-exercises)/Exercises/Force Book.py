5924e2ddccfbff48ec9fc5e6ab9bf261cef9cea5
Eduard Velkov <velkovmail@yandex.ru>
2022-03-11 14:50:33 +0300

    Update Force Book.py

--- Python/Fundamentals/Dictionaries(lab-exercises)/Exercises/Force Book.py
+++ Python/Fundamentals/Dictionaries(lab-exercises)/Exercises/Force Book.py
@@ -1,59 +1,46 @@
 forceBook={}
 while True:
     command=input()
-    if "|" in command:
-        forceBookUser=command.split(" | ")
-        sideName=forceBookUser[0]; username=forceBookUser[1]
-        noUserAndSide=True
-        for j in forceBook:
-            if j==sideName:
-                noUserAndSide=False
-                userFound=False
+    if command!="Lumpawaroo":
+        if "|" in command:
+            command=command.split(" | ")
+            isThereSuchUser=False
+            for j in forceBook:
                 for k in range(0,len(forceBook[j])):
-                    if username==forceBook[j][k]:
-                        userFound=True
+                    if forceBook[j][k]==command[1]:
+                        isThereSuchUser=True
                         break
-                if userFound==False:
-                    forceBook[j].append(username)
-        if noUserAndSide==True:
-            forceBook[sideName]=[username]
-    elif "->" in command:
-        forceBookUser=command.split(" -> ")
-        username=forceBookUser[0]; sideName=forceBookUser[1]
-        noSide=True
-        for j in forceBook:
-            if j==sideName:
-                noSide=False
-        if noSide==True:
-            forceBook[sideName]=[]
-        noUser=True
-        for j in forceBook:
-            try:
-                ind=forceBook[j].index(username)
-                forceBook[sideName].append(username)
-                noUser=False
-                del forceBook[j][ind]
-            except:
+                if isThereSuchUser == True:
+                    break
+            if isThereSuchUser==True:
                 continue
-        if noUser==True:
-            forceBook[sideName].append(username)
-        print(f"{username} joins the {sideName} side!")
-    elif command=="Lumpawaroo":
-        for j in forceBook:
-            forceBook[j].insert(0,len(forceBook[j]))
-        orderDescByForce=sorted(forceBook.items(),key=lambda x: x[1][0], reverse=True)
-        orderDescByForce=dict(orderDescByForce)
-        for j in orderDescByForce:
-            del orderDescByForce[j][0]
-        orderByName=sorted(orderDescByForce.items(), key=lambda x: x[1])
-        orderByName=dict(orderByName)
-        for j in orderByName:
-            members=len(orderByName[j])
-            orderByName[j].sort()
-            if members != 0:
-                print(f"Side: {j}, Members: {members}")
-                for k in range(0,len(orderByName[j])):
-                    print(f"! {orderByName[j][k]}")
             else:
-                continue
-        break
+                if command[0] in forceBook:
+                    forceBook[command[0]].append(command[1])
+                else:
+                    forceBook[command[0]]=[command[1]]
+        elif "->" in command:
+            command=command.split(" -> ")
+            isThereSuchUser=False
+            for j in forceBook:
+                for k in range(0,len(forceBook[j])):
+                    if forceBook[j][k]==command[0]:
+                        isThereSuchUser=True
+                        del forceBook[j][k]
+                        break
+                if isThereSuchUser==True:
+                    break
+            if not command[1] in forceBook:
+                forceBook[command[1]]=[command[0]]
+            else:
+                forceBook[command[1]].append(command[0])
+            print(f"{command[0]} joins the {command[1]} side!")
+    else:
+       for j in forceBook:
+           if len(forceBook[j])!=0:
+               print(f"Side: {j}, Members: {len(forceBook[j])}")
+               for k in range(0,len(forceBook[j])):
+                   print(f"! {forceBook[j][k]}")
+           else:
+             continue
+       break
