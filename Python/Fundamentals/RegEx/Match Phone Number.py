e84df87b302023c5d5fe42291c47c90dbcea5b4a
Eduard Velkov <velkovmail@yandex.ru>
2022-03-15 00:10:14 +0200

    Update Match Phone Number.py

--- Python/Fundamentals/RegEx/Match Phone Number.py
+++ Python/Fundamentals/RegEx/Match Phone Number.py
@@ -1,5 +1,10 @@
 import re
+validPhone=re.compile(r"(\+359\s2\s[0-9]{3}\s[0-9]{4}\b)|(\+359-2-[0-9]{3}-[0-9]{4}\b)")
 phoneNumbers=input()
-pattern=re.compile(r"^[ ]*\+359[ |-]2[ |-][0-9]{3}[ |-][0-9]{4}")
-matches=pattern.findall(phoneNumbers)
-print(",".join(matches))
+res=validPhone.findall(phoneNumbers)
+for k in range(0,len(res)):
+    for j in range(0,len(res[k])):
+        if res[k][j]!="":
+            res[k]=res[k][j]
+            break
+print(', '.join(res))
