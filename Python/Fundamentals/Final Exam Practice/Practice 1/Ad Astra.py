e995023d00df7db7226a6255788c32457d9c8f5f
Eduard Velkov <velkovmail@yandex.ru>
2022-03-28 19:12:35 +0300

    Update Ad Astra.py

--- Python/Fundamentals/Final Exam Practice/Practice 1/Ad Astra.py
+++ Python/Fundamentals/Final Exam Practice/Practice 1/Ad Astra.py
@@ -1,13 +1,11 @@
 import re
-text=input()
-validFoodInfo=re.compile(r"[#|\|]{1}(?P<itemName>[A-Za-z\s]+)[#|\|]{1}(?P<expiry>[0-9]{2}/[0-9]{2}/[0-9]{2})[#|\|]{1}(?P<calories>[0-9]+)[#|\|]{1}")
-totalCals=0
-foodData=validFoodInfo.findall(text)
-outputFoodData=""
-for k in range(0,len(foodData)):
-    totalCals+=int(foodData[k][2])
-    outputFoodData+=f"Item: {foodData[k][0]}, Best before: {foodData[k][1]}, Nutrition: {foodData[k][2]}"
-    if k!=len(foodData)-1:
-        outputFoodData+="\n"
-print(f"You have food to last you for: {totalCals//2000} days!")
-print(outputFoodData)
+singleStr=input()
+totalCal=0; survivalDays=0
+findFoodData=re.compile(r"(?P<FirstSymb>[|\|#])([A-Za-z\s]+)(?P=FirstSymb)([0-9]{2}/[0-9]{2}/[0-9]{2})(?P=FirstSymb)([0-9]{1,5})(?P=FirstSymb)")
+found=findFoodData.findall(singleStr)
+for k in range(0,len(found)):
+    totalCal+=int(found[k][3])
+survivalDays=totalCal//2000
+print(f"You have food to last you for: {survivalDays} days!")
+for k in range(0,len(found)):
+    print(f"Item: {found[k][1]}, Best before: {found[k][2]}, Nutrition: {found[k][3]}")
