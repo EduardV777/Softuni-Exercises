8b1a36452af2ff5a99428eb39460727d8746bf98
Eduard Velkov <velkovmail@yandex.ru>
2022-03-30 03:20:45 +0300

    Update The Imitation Game.py

--- Python/Fundamentals/Final Exam Practice/Practice 1/The Imitation Game.py
+++ Python/Fundamentals/Final Exam Practice/Practice 1/The Imitation Game.py
@@ -1,18 +1,17 @@
 import re
 encryptedMsg=input()
-encryptedMsg=[char for char in encryptedMsg]
+encryptedMsg=[e for e in encryptedMsg]
 while True:
     command=input()
     if command!="Decode":
         if "Move" in command:
             command=command.split("|")
-            command[1]=int(command[1])
+            count=int(command[1])
             letters=[]
-            if command[1]<len(encryptedMsg):
-                while command[1]!=0:
-                    letters.append(encryptedMsg[0])
-                    del encryptedMsg[0]
-                    command[1]-=1
+            while count!=0:
+                letters.append(encryptedMsg[0])
+                del encryptedMsg[0]
+                count-=1
             for k in range(0,len(letters)):
                 encryptedMsg.append(letters[k])
         elif "Insert" in command:
@@ -22,16 +21,9 @@ while True:
         elif "ChangeAll" in command:
             command=command.split("|")
             subStr=command[1]; replacement=command[2]
-            findSubStr=re.compile(rf"{subStr}")
             encryptedMsg=''.join(encryptedMsg)
-            foundStrings=findSubStr.finditer(encryptedMsg)
-            encryptedMsg=[char for char in encryptedMsg]
-            for j in foundStrings:
-                indexes=j.span()
-                startInd=indexes[0]; endInd=indexes[1]
-                while startInd<endInd:
-                    encryptedMsg[startInd]=replacement
-                    startInd+=1
+            encryptedMsg=encryptedMsg.replace(subStr,replacement)
+            encryptedMsg=[e for e in encryptedMsg]
     else:
         print(f"The decrypted message is: {''.join(encryptedMsg)}")
         break
