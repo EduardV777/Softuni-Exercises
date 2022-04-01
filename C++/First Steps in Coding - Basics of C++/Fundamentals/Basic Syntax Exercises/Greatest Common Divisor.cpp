4c4c5f66acadd87f1e89106210775cb8ec5c6ed4
Eduard Velkov <velkovmail@yandex.ru>
2022-04-02 01:47:04 +0300

    Update Greatest Common Divisor.cpp

--- C++/First Steps in Coding - Basics of C++/Fundamentals/Basic Syntax Exercises/Greatest Common Divisor.cpp
+++ C++/First Steps in Coding - Basics of C++/Fundamentals/Basic Syntax Exercises/Greatest Common Divisor.cpp
@@ -3,28 +3,24 @@ using namespace std;
 int main() {
 	int a,b,k=10, div1=0, div2=-1;
 	cin >> a >> b;
-	if (a != b) {
-		while (k) {
-			if (a%k == 0) {
-				div1 = k;
-			}
-			if (b%k == 0) {
-				div2 = k;
-			}
-			if (div1 == div2) {
-				break;
-			}
-			k--;
+
+	while (k) {
+		if (a%k == 0) {
+			div1 = k;
 		}
-		if (div1 == 1 && div2 == 1) {
-			cout << a << " and " << b << " are prime numbers, meaning they only divide by 1 and themselves, so their GCD is 1";
+		if (b%k == 0) {
+			div2 = k;
 		}
-		else {
-			cout << div1 << " is the largest number that divides both " << a << " and " << b << " (without remainder)";
+		if (div1 == div2) {
+			break;
 		}
+		k--;
+	}
+	if (a == b) {
+		cout << a;
 	}
 	else {
-		cout << "Both numbers are " << a << ", so GCD is " << a;
+		cout << div1;
 	}
 	return 0;
 }
