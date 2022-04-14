9145e557e22f47b0f25c47464357e54c3f3f66bc
Eduard Velkov <velkovmail@yandex.ru>
2022-04-14 12:41:26 +0300

    Create Closest Numbers.cpp

---
+++ C++/First Steps in Coding - Basics of C++/Fundamentals/Arrays - Exercises/Closest Numbers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int* CreateArray(int size) {
+	int* arr = new int[size];
+	for (int k = 0; k < size; k++) {
+		cin >> arr[k];
+	}
+	return arr;
+}
+
+int FindClosest(int* arr, int size) {
+	int currDiff = -777;
+	for (int k = 0; k < size; k++) {
+		for (int j = 0; j < size; j++) {
+			if (j == k) {
+				continue;
+			}
+			if (currDiff>abs(arr[k] - arr[j]) || currDiff==-777) {
+				currDiff = abs(arr[k] - arr[j]);
+			}
+		}
+	}
+	return currDiff;
+}
+
+int main() {
+	int size, *numbers;
+	cin >> size;
+	numbers = CreateArray(size);
+	cout << FindClosest(numbers, size);
+	return 0;
+}
