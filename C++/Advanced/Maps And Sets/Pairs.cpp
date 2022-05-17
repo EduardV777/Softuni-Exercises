a9ebae84595310023a13c6d8d66e3ea48d0d9869
EduardV777 <velkovmail@yandex.ru>
2022-05-17 19:38:59 +0300

    C++ Advanced Pairs

--- C++/Advanced/Maps And Sets/Pairs.cpp
+++ C++/Advanced/Maps And Sets/Pairs.cpp
@@ -1,10 +1,25 @@
 #include <utility>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-	std::pair<int, string> pairVar(35988000000, "Eduard Velkov");
-	cout << "Pair variable values: \n"<<pairVar.first<<" - "<<pairVar.second<<"\n";
+	pair<int, string> pairVar(35945213, "Eduard Velkov");
+	cout << "Pair variable values: \nPhone/Name: " << pairVar.first << " - " << pairVar.second << "\n";
+
+	cout << " --------------- \nCreate new pair\nEnter account name: ";
+
+	string accName; int id;
+	getline(cin >> ws, accName);
+	cout << "\nEnter account id: ";
+	cin >> id;
+	pair<string, int> account;
+	account.first = accName; account.second = id;
+	cout << "\n\nAccount name: " << account.first << "\nID: " << account.second << "\n";
+	cout << " --------------- \n";
+	pair<string, string> location = { "Sofia","Nadezhda" };
+
+	cout << location.first << ", " << location.second << endl;
 	return 0;
 }
