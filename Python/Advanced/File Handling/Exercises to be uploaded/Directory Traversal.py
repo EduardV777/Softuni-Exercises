9bc7ac48c22aeb1459baaf5c0dbacd3a2cb430e4
EduardV777 <velkovmail@yandex.ru>
2022-06-14 22:16:42 +0300

    Python Advanced File Handling Exercises

--- Python/Advanced/File Handling/Exercises to be uploaded/Directory Traversal.py
+++ Python/Advanced/File Handling/Exercises to be uploaded/Directory Traversal.py
@@ -1,1 +1,30 @@
-#to be done
+import os
+
+foundFilesByExtensions = {}
+output = ""
+
+filesInFolder = os.listdir('./Example')
+
+for k in range(0,len(filesInFolder)):
+    fileName = filesInFolder[k]
+    fileName = fileName.split(".")
+
+    extension = "."+fileName[1]
+
+    if extension not in foundFilesByExtensions:
+        foundFilesByExtensions[extension] = [fileName[0]+extension]
+    else:
+        foundFilesByExtensions[extension].append(fileName[0]+extension)
+
+foundFilesByExtensions = foundFilesByExtensions.items()
+
+foundFilesByExtensions = sorted(foundFilesByExtensions, key=lambda x: x[0])
+
+foundFilesByExtensions = dict(foundFilesByExtensions)
+
+for ext in foundFilesByExtensions:
+    print(ext)
+    foundFilesByExtensions[ext].sort()
+    
+    for file in foundFilesByExtensions[ext]:
+        print(f"- - - {file}")
