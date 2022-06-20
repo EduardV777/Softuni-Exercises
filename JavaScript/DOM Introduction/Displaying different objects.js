1a6e94e8ca92c576cd59b041f93fb5fb076ad855
EduardV777 <velkovmail@yandex.ru>
2022-06-20 13:44:29 +0300

    JS Advanced (Testing vsCode commit)

---
+++ JavaScript/DOM Introduction/Displaying different objects.js
@@ -0,0 +1,5 @@
+console.dir(window);
+console.dir(screen);
+console.dir(location);
+console.dir(history);
+console.dir(navigator);
---
+++ JavaScript/DOM Introduction/Getting elements by tag and class.js
@@ -0,0 +1,7 @@
+pTags = document.getElementsByTagName('p');
+randClassElements = document.getElementsByClassName('rand');
+spanElement = document.querySelector('span.rand');
+
+console.log(pTags);
+console.log(randClassElements);
+console.log(spanElement);
