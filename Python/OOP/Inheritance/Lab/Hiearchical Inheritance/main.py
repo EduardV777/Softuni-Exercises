ea149d4e6780c8e197ea72b0cddac50c14f668f2
EduardV777 <velkovmail@yandex.ru>
2022-07-08 22:43:39 +0300

    Python OOP Inheritance Lab

---
+++ Python/OOP/Inheritance/Lab/Hiearchical Inheritance/main.py
@@ -0,0 +1,12 @@
+from project.cat import Cat
+from project.dog import Dog
+
+
+#Test code
+"""
+cat1 = Cat()
+dog1 = Dog()
+
+print(cat1.eat())
+print(dog1.eat())
+"""
---
+++ Python/OOP/Inheritance/Lab/Single Inheritance/main.py
@@ -0,0 +1,8 @@
+from project.dog import Dog
+
+
+#Test code
+"""
+obj1 = Dog()
+print(obj1.eat())
+"""
