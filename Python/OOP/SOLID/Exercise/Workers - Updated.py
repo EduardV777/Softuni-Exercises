9c0a2bba6e2d53d383e98e2f83434433ee26ba06
EduardV777 <velkovmail@yandex.ru>
2022-07-28 22:34:42 +0300

    Python OOP SOLID Exercise

---
+++ Python/OOP/SOLID/Exercise/Workers - Updated.py
@@ -0,0 +1,93 @@
+"""
+[ISP]
+You are provided with a code on which you have to apply the ISP (Interface Segregation Principle) by splitting 
+the Worker class into two classes (Workable and Eatable), so the Robot class no longer needs to implement
+the eat method.
+"""
+
+
+from abc import ABC, abstractmethod
+import time
+
+class Workable(ABC):
+
+    @abstractmethod
+    def work(self):
+        pass
+
+class Eatable(ABC):
+
+    @abstractmethod
+    def eat(self):
+        pass
+    
+    
+# class AbstractWorker:
+#     __metaclass__ = ABCMeta
+
+#     @abstractmethod
+#     def work(self):
+#         pass
+
+#     @abstractmethod
+#     def eat(self):
+#         pass
+
+class Worker(Workable, Eatable):
+
+    def work(self):
+        print("I'm normal worker. I'm working.")
+
+    def eat(self):
+        print("Lunch break....(5 secs)")
+        time.sleep(5)
+
+
+
+class SuperWorker(Workable, Eatable):
+
+    def work(self):
+        print("I'm super worker. I work very hard!")
+
+    def eat(self):
+        print("Lunch break....(3 secs)")
+        time.sleep(3)
+
+
+class Manager:
+
+    def __init__(self):
+        self.worker = None
+
+    def set_worker(self, worker):
+        assert isinstance(worker, Workable), "`worker` must be of type {}".format(Workable)
+
+        self.worker = worker
+
+    def manage(self):
+        self.worker.work()
+
+    def lunch_break(self):
+        self.worker.eat()
+
+class Robot(Workable):
+
+    def work(self):
+        print("I'm a robot. I'm working....")
+
+    # def eat(self):
+    #     print("I don't need to eat....")
+
+
+manager = Manager()
+manager.set_worker(Worker())
+manager.manage()
+manager.lunch_break()
+
+manager.set_worker(SuperWorker())
+manager.manage()
+manager.lunch_break()
+
+manager.set_worker(Robot())
+manager.manage()
+manager.lunch_break()
--- Python/OOP/SOLID/Exercise/workers.py
+++ Python/OOP/SOLID/Exercise/workers.py
@@ -4,34 +4,37 @@ You are provided with a code on which you have to apply the DIP (Dependency Inve
 adding new worker classes, the Manager class will work properly.
 """
 
-class Worker:
+from abc import ABC, abstractmethod
+
+class WorkerToAdd(ABC):
+
+    @abstractmethod
+    def work(self):
+        pass
+
+class Worker(WorkerToAdd):
 
     def work(self):
         print("I'm working!!")
 
 
-class SuperWorker(Worker):
-
-    def work(self):
-        print("I work very hard!!!")
-
-class LazyWorker(Worker):
-
-    def work(self):
-        print("I dont work")
-
 class Manager:
 
     def __init__(self):
-        pass
+        self.worker = None
 
-    def set_worker(self, worker: Worker):
+    def set_worker(self, worker: WorkerToAdd):
         #assert isinstance(worker, Worker), '`worker` must be of type {}'.format(Worker)
         self.worker = worker
 
     def manage(self):
-        if self.worker is not None:
+        if self.worker != None:
             self.worker.work()
+
+class SuperWorker(WorkerToAdd):
+
+    def work(self):
+        print("I work very hard!!!")
 
 
 
@@ -41,12 +44,8 @@ manager.set_worker(worker)
 manager.manage()
 
 super_worker = SuperWorker()
-lazy_worker = LazyWorker()
 try:
     manager.set_worker(super_worker)
     manager.manage()
-    manager.set_worker(lazy_worker)
-    manager.manage()
-
 except AssertionError:
     print("manager fails to support super_worker....")
