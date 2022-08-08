f0fe0f44c1dc28b138ce800e292712603a201fea
EduardV777 <velkovmail@yandex.ru>
2022-08-08 14:40:43 +0300

    Javascript Advanced Update Syntax, Functions, Statements

--- JavaScript/Advanced/Syntax, Functions, Statements/Exercise/Road Radar.js
+++ JavaScript/Advanced/Syntax, Functions, Statements/Exercise/Road Radar.js
@@ -1,28 +1,54 @@
 function IsWithinLimit(speed, area){
-  if(area=="city"){
-    speedLimit=50;
-  }
-  else if(area=="motorway"){
-    speedLimit=130;
-  }
-  else if(area=="interstate"){
-    speedLimit=90;
-  }
-  else if(area=="residential"){
-    speedLimit=20;
+  let speedLimit, speedStatus;
+  function CheckSpeed(){
+    switch(area){
+      case "motorway":
+        speedLimit = 130;
+        if(speed > speedLimit){
+          return true;
+        }else {
+          return false;
+        }
+      case "interstate":
+        speedLimit = 90;
+        if(speed > speedLimit){
+          return true;
+        }else {
+          return false;
+        }
+      case "city":
+        speedLimit = 50;
+        if(speed > speedLimit){
+          return true;
+        }else {
+          return false;
+        }
+      case "residential":
+        speedLimit = 20;
+        if(speed > speedLimit){
+          return true;
+        }else {
+          return false;
+        }
+    }
   }
 
-  if(speed<=speedLimit){
-    console.log("Driving "+speed+" km/h in a "+speedLimit+" zone")
-  }else {
-    diff=speed-speedLimit;
-    if(diff<=20){
-      status="speeding";
-    }else if(diff<=40){
-      status="excessive speeding";
-    }else{
-      status="reckless driving";
+  if(CheckSpeed()){
+    let speedingAmount = speed - speedLimit;
+    if(speedingAmount <= 20){
+        speedStatus = "speeding";
+    } else if (speedingAmount <= 40){
+      speedStatus = "excessive speeding";
+    } else {
+      speedStatus = "reckless driving";
     }
-    console.log("The speed is "+diff+" km/h faster than the allowed speed of "+speedLimit+" - "+status);
+    console.log(`The speed is ${speedingAmount} km/h faster than the allowed speed of ${speedLimit} - ${speedStatus}`);
+  }else {
+    console.log(`Driving ${speed} km/h in a ${speedLimit} zone`);
   }
 }
+
+IsWithinLimit(40, 'city');
+IsWithinLimit(21, 'residential');
+IsWithinLimit(120, 'interstate');
+IsWithinLimit(200, 'motorway');
