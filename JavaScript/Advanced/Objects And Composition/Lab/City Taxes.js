9afdcab59e613c8e6853071a2386b2b78bcaf321
EduardV777 <velkovmail@yandex.ru>
2022-08-10 01:46:09 +0300

    JS Advanced Object And Composition

--- JavaScript/Advanced/Objects And Composition/Lab/City Taxes.js
+++ JavaScript/Advanced/Objects And Composition/Lab/City Taxes.js
@@ -1,8 +1,31 @@
-function cityTaxes(cityName, population, treasury){
-  cityData={'name':cityName, 'population': population, 'treasury':treasury, 'taxRate': 10,
-            'collectTaxes':function() { this.treasury=Math.floor(eval(this.population*this.taxRate));},
-            'applyGrowth':function(pct){ this.population=Math.floor(eval(this.population*(pct/100)));},
-            'applyRecession':function(pct){this.treasury=Math.floor(eval(this.treasury-(this.treasury*(pct/100))));} };
+function cityTaxes(name, population, treasury){
+  let obj = {name: name, population: population, treasury: treasury, taxRate: 10,
+    collectTaxes: function() {
+      this.treasury += Math.floor(this.population * this.taxRate);
+    },
+    applyGrowth(pct){
+      this.population += Math.floor(this.population * (pct / 100));
+    },
+    applyRecession(pct){
+      this.treasury -= Math.floor(this.treasury * (pct / 100));
+    }
+  };
 
-  return cityData;
+  return obj;
+
 }
+
+// const city = 
+//   cityTaxes('Tortuga',
+//   7000,
+//   15000);
+// console.log(city);
+
+const city =
+  cityTaxes('Tortuga',
+  7000,
+  15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
--- JavaScript/Advanced/Objects And Composition/Lab/Object Factory.js
+++ JavaScript/Advanced/Objects And Composition/Lab/Object Factory.js
@@ -1,41 +1,79 @@
-// function ComposeObjects(funcLibrary, orders) {
-//   let objects=[]
-//   console.log(orders.parts)
-//   for(var k in orders.parts){
-//     console.log(orders.parts[k]);
-//     objects.push(funcLibrary.k(orders.template));
-//   }
-//   return objects;
-// }
-//
-//
+function factory(libraryFunc, ordersArr){
+    let completedOrders = [];
+
+    for(var k = 0; k < ordersArr.length; k++){
+        let name = ordersArr[k].template.name;
+        let currOrder = {name};
+
+        for(var j of ordersArr[k].parts){
+            for(var key in libraryFunc){
+                if(key == j){
+                    console.log(j);
+                    currOrder[`${j}`] = libraryFunc[j];
+                    break
+                }
+            }
+        }
+        completedOrders.push(currOrder);
+    }
+
+    return completedOrders;
+}
+
 // const library = {
-// print: function () {
-// console.log(`${this.name} is printing a page`);
-// },
-// scan: function () {
-// console.log(`${this.name} is scanning a document`);
-// },
-// play: function (artist, track) {
-// console.log(`${this.name} is playing '${track}' by ${artist}`);
-// },
-// };
-// const orders = [
-// {
-// template: { name: 'ACME Printer'},
-// parts: ['print']
-// },
-// {
-// template: { name: 'Initech Scanner'},
-// parts: ['scan']
-// },
-// {
-// template: { name: 'ComTron Copier'},
-// parts: ['scan', 'print']
-// },
-// {
-// template: { name: 'BoomBox Stereo'},
-// parts: ['play']
+//     print: function () {
+//       console.log(`${this.name} is printing a page`);
+//     },
+//     scan: function () {
+//       console.log(`${this.name} is scanning a document`);
+//     },
+//     play: function (artist, track) {
+//       console.log(`${this.name} is playing '${track}' by ${artist}`);
+//     },
+//   };
+//   const orders = [
+//     {
+//       template: { name: 'ACME Printer'},
+//       parts: ['print']      
+//     },
+//     {
+//       template: { name: 'Initech Scanner'},
+//       parts: ['scan']      
+//     },
+//     {
+//       template: { name: 'ComTron Copier'},
+//       parts: ['scan', 'print']      
+//     },
+//     {
+//     template: { name: 'BoomBox Stereo'},
+//     parts: ['play']      
 //   }
 // ];
-// ComposeObjects(library, orders);
+// const products = factory(library, orders);
+// console.log(products);
+
+
+//const factory = result;
+
+const library = {
+    get1: function () {
+        return 1;
+    },
+    get3: function () {
+        return 3;
+    },
+};
+
+const orders = [
+    {
+        template: {},
+        parts: ['get1']
+    },
+    {
+        template: {},
+        parts: ['get1', 'get3']
+    },
+];
+
+const products = factory(library, orders);
+console.log(products);
