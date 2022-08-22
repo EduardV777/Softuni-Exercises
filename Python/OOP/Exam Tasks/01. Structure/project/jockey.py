cc7df523cd01c87a48ecdfcf533d66a8cee0ea79
Eduard Velkov <velkovmail@yandex.ru>
2022-08-21 22:57:10 -0700

    Add files via upload

---
+++ Python/OOP/Exam Tasks/01. Structure/project/jockey.py
@@ -0,0 +1,29 @@
+
+class Jockey:
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        self.horse = None
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, val):
+        if val.strip() == "":
+            raise ValueError("Name should contain at least one character!")
+        else:
+            self.__name = val
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, val):
+        if val < 18:
+            raise ValueError("Jockeys must be at least 18 to participate in the race!")
+        else:
+            self.__age = val
